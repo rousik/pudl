e8594a87c4088c79e4fef7aed2fe2b6632c9c01f
Jan Rous <rousik@gmail.com>
2020-03-19 17:30:39 -0600

    Add more excel tests.
    Also add some more doxy strings so that Codacy doesn't complain.

--- src/pudl/extract/excel_test.py
+++ src/pudl/extract/excel_test.py
@@ -3,6 +3,7 @@ import unittest
 import unittest.mock as mock
 from unittest.mock import patch
 
+import numpy as np
 import pandas as pd
 
 import pudl.extract.excel as excel
@@ -59,6 +60,48 @@ def _fake_data_frames(page_name, **kwargs):
     return fake_data[page_name]
 
 
+class FakeMetadata(excel.Metadata):
+    """This provides fake metadata suitable for testing.
+
+    Both skiprows and sheet_name return 0 for everything but this
+    object can keep list of known pages and return column maps.
+    """
+
+    def __init__(self, dataset_name, **kwargs):
+        """Set up FakeMetadata instance.
+
+        Args:
+            dataset_name (str): name of the dataset.
+            **kwargs: maps each page to its column map
+        """
+        self._dataset_name = dataset_name
+        self._column_maps = {}
+        for page, column_map in kwargs.items():
+            self._column_maps[page] = column_map
+
+    @staticmethod
+    def get_skiprows(year, page):
+        """Returns 0 for everything."""
+        return 0
+
+    @staticmethod
+    def get_sheet_name(year, page):
+        """Returns 0 for everything."""
+        return 0
+
+    def get_column_map(self, year, page):
+        """Returns column mapping for given page."""
+        return self._column_maps[page]
+
+    def get_all_columns(self, page):
+        """Returns list of all pudl columns for a given page."""
+        return sorted(self._column_maps[page].values())
+
+    def get_all_pages(self):
+        """Returns list of all pages."""
+        return sorted(self._column_maps.keys())
+
+
 class TestGenericExtractor(unittest.TestCase):
     """Test operation of the excel.GenericExtractor class."""
 
@@ -96,5 +139,27 @@ class TestGenericExtractor(unittest.TestCase):
             }),
             dfs['boxes'])
 
-    # TODO(rousik@gmail.com): need to figure out how to test process_$x methods.
-    # TODO(rousik@gmail.com): we should test that empty columns are properly added.
+    @patch('pudl.extract.excel.pd.read_excel')
+    def test_missing_columns_added(self, fake_reader):
+        """Test that missing "description" column is added to final page."""
+        extractor = FakeExtractor(
+            '/blah',
+            metadata=FakeMetadata(
+                'fake',
+                first_page={
+                    'orig_name': 'name', 'orig_year': 'year',
+                    'orig_desc': 'description'
+                }))
+        fake_reader.return_value = pd.DataFrame(
+            data={'orig_name': ['A'], 'orig_year': ['B']})
+        dfs = extractor.extract([2010])
+        self.assertEqual(['first_page'], list(dfs.keys()))
+        print(dfs['first_page'])
+        pd.testing.assert_frame_equal(
+            pd.DataFrame(data={
+                'description': [np.nan],
+                'name': ['A'], 'year': ['B']
+            }).astype({'description': 'object'}),
+            dfs['first_page'])
+
+    # TODO(rousik@gmail.com): test correct processor operation.
